f9fcdea9b2bfbd927dbc5ae5e02c1e8adcd8832e
ErickGabriel55 <erickgabrie56@gmail.com>
2026-01-06 23:19:32 -0300

    06-01-2026 23:19

--- index.php
+++ index.php
@@ -42,11 +42,22 @@
                 </div>
 
                 
-                <?if (isset($_GET['login']) && $_GET['login'] == 'erro') { echo $_GET['login'];?>
-                  <div class="text-danger">
-                    Usuário ou senha invalido(s)
-                  </div>
-                <? } ?>
+               <?php
+                  if (isset($_GET['login']) && $_GET['login'] === 'erro') {
+                ?>
+                    <div class="text-danger">
+                      Usuário ou senha inválido(s)
+                    </div>
+                <?php
+                  } elseif (isset($_GET['login']) && $_GET['login'] === 'erro2') {
+                ?>
+                    <div class="text-danger">
+                      Faça login antes de acessar as páginas protegidas!
+                    </div>
+                <?php
+                  }
+                ?>
+
 
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
--- valida_login.php
+++ valida_login.php
@@ -1,37 +1,39 @@
 <?php
-    // variavel que verifica se a autenticação foi realizada
-    $usuarios_autenticado = false;
+session_start();
 
+// Simulação de banco de dados
+$usuarios_app = array(
+    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+    array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+);
 
-    // usuarios do sistmea
-    $usuarios_app = array(
-        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
-    );
+$usuario_autenticado = false;
 
-    /*echo '<pre>';
-    print_r($usuarios_app);
-    echo '</pre>';*/
-
-    foreach($usuarios_app as $user){
-       /*echo 'Usuário app: ' . $user['email'] . ' / ' . $user['senha'];
-        echo '<br>Usuário form: ' .  $_POST['email'] . '/' . $_POST['senha'];*/
-
-        // Verifica se o usuário existe no "banco de dados" e se ele digitou corretamente as informações
-        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){ 
-            $usuarios_autenticado = true;
-        }
-        
-        /* caso essa variavel seja true exibe a mensagem,  do contrario retorna pra index novamente com uma mensagem de erro*/
-        if($usuarios_autenticado){ 
-            echo '<br>';
-            echo 'Usuario autenticado!';
-        }else {
-            header('Location: index.php?login=erro');
-        }
-        echo '<hr>';
-    }
-
+// Verifica se os dados foram enviados
+if(isset($_POST['email']) && isset($_POST['senha'])) {
     
-    // $_POST['senha'];
+    foreach($usuarios_app as $user) {
+        // Verifica se o usuário existe no "banco de dados"
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+            break; // Para o loop quando encontrar o usuário
+        }
+    }
+    
+    if($usuario_autenticado) {
+        $_SESSION['autenticado'] = 'SIM';
+        $_SESSION['email'] = $_POST['email'];
+        header('Location: home.php');
+        exit(); // Importante: sempre usar exit() após header redirect
+    } else {
+        $_SESSION['autenticado'] = 'NAO';
+        header('Location: index.php?login=erro');
+        exit();
+    }
+    
+} else {
+    // Se não houver POST, redireciona para o login
+    header('Location: index.php');
+    exit();
+}
 ?>

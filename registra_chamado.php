7ad0b36dae3c5ebaf16bf68a48e5b4a0c1a6519f
ErickGabriel55 <erickgabrie56@gmail.com>
2026-02-04 09:38:31 -0300

    04-01-2026 09:38

---
+++ registra_chamado.php
@@ -0,0 +1,8 @@
+<?php 
+    //fopen('arquivo.hd', 'a');
+    $titulo = str_replace('#', '-', $_POST['titulo']);
+    $categoria = str_replace('#', '-', $_POST['categoria']);
+    $descricao = str_replace('#', '-', $_POST['descricao']);
+    $texto = $titulo . '#' . $categoria . '#' . $descricao;
+
+?>

fa2cd63dabd15f4000c6b3d32a2cbd55db4277e7
ErickGabriel55 <erickgabrie56@gmail.com>
2026-02-10 09:38:56 -0300

    10-02-2026 09:38

--- registra_chamado.php
+++ registra_chamado.php
@@ -1,8 +1,20 @@
 <?php 
-    //fopen('arquivo.hd', 'a');
+    // Montagem do texto
     $titulo = str_replace('#', '-', $_POST['titulo']);
     $categoria = str_replace('#', '-', $_POST['categoria']);
     $descricao = str_replace('#', '-', $_POST['descricao']);
-    $texto = $titulo . '#' . $categoria . '#' . $descricao;
+
+    //implode('#", $_POST)
+
+    
+    $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+    
+    // Abrindo o arquivo
+    $arquivo = fopen('arquivo.hd', 'a');
+    // Escrevendo no arquivo
+    fwrite($arquivo, $texto);
+    // Fechando o arquivo
+    fclose($arquivo);
+    header('Location: abrir_chamado.php');
 
 ?>
